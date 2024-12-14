1f9912d9fbd77c487b5e2275bb000f1a46436a79
Ly Van Minh <lyvanminh280504@gmail.com>
2024-12-14 14:33:21 +0700

    no desc

--- app/src/main/java/com/example/btl_mobile/MainActivity.java
+++ app/src/main/java/com/example/btl_mobile/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.btl_mobile;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+//import android.widget.Button;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.btl_mobile.R;
+import com.example.btl_mobile.actIndex;
 
 public class MainActivity extends AppCompatActivity {
+    // Bước 1: Khai báo các thành phần giao diện
+    Button btnIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setContentView(R.layout.loginconnectify);
+        btnIndex = findViewById(R.id.btnIndex);
+
+        btnIndex.setOnClickListener(v -> {
+            Intent it = new Intent(MainActivity.this, actIndex.class);
+            startActivity(it);
+            finish(); //
         });
     }
 }
---
+++ app/src/main/java/com/example/btl_mobile/actIndex.java
@@ -0,0 +1,27 @@
+package com.example.btl_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.btl_mobile.R;
+
+public class actIndex extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.indexconnecttify);
+
+
+    }
+}
